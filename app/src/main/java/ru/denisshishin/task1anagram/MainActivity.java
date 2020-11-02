98f888cea4e2f6ab73a134735da6a5bc9de4234a
Denis <sh-kin@mail.ru>
2020-11-02 17:24:39 +0300

    Task 1 Foxminded.v3 1. Заменил конкатенацию сложение строк на StringBuilder 2. Добавил обработку переворота экрана 3. Добавил toast,если editText пустой.

--- app/src/main/java/ru/denisshishin/task1anagram/MainActivity.java
+++ app/src/main/java/ru/denisshishin/task1anagram/MainActivity.java
@@ -1,19 +1,27 @@
 package ru.denisshishin.task1anagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.security.Key;
+
 public class MainActivity extends AppCompatActivity {
     TextView tvOutput;
-    TextInputEditText editText;
-    TextInputEditText editTextExceptions;
+    EditText editText, editTextExceptions;
     Button btnReverse;
+    //String anagram;
+    final static String KEY = "KEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +33,40 @@ public class MainActivity extends AppCompatActivity {
         editTextExceptions = findViewById(R.id.tietInputExceptionText);
         btnReverse = findViewById(R.id.btnReverse);
 
-        btnReverse.setOnClickListener(v ->
-                tvOutput.setText(Anagram.createAnagram(editText.getEditableText().toString(),
-                        editTextExceptions.getEditableText().toString())));
+        /*anagram = Anagram.createAnagram(editText.getText().toString(),
+        editTextExceptions.getText().toString());
+
+        btnReverse.setOnClickListener(v -> {
+            tvOutput.setText(anagram);
+        });*/
+
+          btnReverse.setOnClickListener(v -> {
+              if (!editText.getText().toString().isEmpty()) {
+                  tvOutput.setText(
+                          Anagram.createAnagram(editText.getText().toString(),
+                          editTextExceptions.getText().toString()));
+              }
+              else {
+                  Toast toast = Toast.makeText(getApplicationContext(),"Вы ничего не ввели",
+                          Toast.LENGTH_SHORT);
+                  toast.show();
+              }
+          });
     }
+
+     @Override
+   protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(KEY,Anagram.createAnagram(editText.getEditableText().toString(),
+                editTextExceptions.getEditableText().toString()));
     }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        tvOutput.setText(Anagram.createAnagram(editText.getEditableText().toString(),
+                editTextExceptions.getEditableText().toString()));
+    }
+}
 
 
